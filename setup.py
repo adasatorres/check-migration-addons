43d8ec59074b5ed438f48025eea19295cfa20b0f
Adasat Torres de León <adasat.torres.dev@gmail.com>
2025-08-15 22:38:17 +0100

    [IMP] docsctrings

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
-#setup.py
-from setuptools import setup, find_packages
+"""
+setup.py
 
+This script is used for packaging the 'check_migration_addons' Python project.
+It utilizes setuptools to define the package metadata and dependencies.
+
+Package Information:
+- Name: check_migration_addons
+- Version: 1.0.0
+- Author: Adasat Torres de León
+- Author Email: adasat.torres.dev@gmail.com
+- Description: Verifies migrated addons in GitHub repositories and generates Excel reports.
+- URL: https://github.com/adasatorres/check-migration-addons
+
+Dependencies:
+- requests (>=2.30.0)
+- openpyxl (>=3.1.0)
+- pandas (>=2.1.0)
+
+Entry Points:
+- Console script: check-migration-addons, which executes the main function from src.main module.
+"""
+from setuptools import setup, find_packages
 
 setup(
     name="check_migration_addons",
--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+"""
+This script verifies the existence of specific directories in GitHub repositories
+based on a list provided in an input Excel file. It utilizes the GitHub API to check
+the status of directories in specified branches of the repositories.
+
+The script performs the following key functions:
+1. Initializes configuration settings from a 'config.ini' file.
+2. Parses command-line arguments to obtain the input file, branch name, and GitHub token.
+3. Reads an Excel file containing a list of repositories and their corresponding directories.
+4. Checks if the specified directories exist in the given repositories and branches.
+5. Outputs the results to the console and saves them to a new Excel file.
+
+Usage:
+    python main.py --input-file <input_file_path> --branch <branch_name> --token <github_token>
+
+Dependencies:
+    - pandas
+    - requests
+    - openpyxl
+"""
+
 import argparse
 import configparser
 import importlib.resources
@@ -9,12 +30,34 @@ import os
 config = configparser.ConfigParser()
 
 def init_config() -> None:
+    """
+    Initializes the configuration by loading settings from the 'config.ini' file.
+
+    This function reads the configuration file located in the 'src' directory
+    and prints the sections of the configuration that have been loaded.
+
+    Returns:
+        None: This function does not return any value.
+    """
     with importlib.resources.path('src', 'config.ini') as config_path:
         config.read(config_path)
         print("Configuración cargada:", config.sections())
-
+    
 
 def get_arguments() -> dict:
+    """
+    Parses command-line arguments for the GitHub repository verification script.
+
+    This function uses argparse to handle input from the command line, requiring
+    the user to specify an input file containing a list of repositories, a branch name,
+    and a personal access token for GitHub.
+
+    Returns:
+        dict: A dictionary containing the parsed command-line arguments.
+            - 'input-file': The input file with the list of repositories (str).
+            - 'branch': The name of the branch (str).
+            - 'token': The personal access token for GitHub (str).
+    """
     parser = argparse.ArgumentParser(description="Verifica directorios en repositorios de GitHub desde un fichero y exporta resultados a Excel")
     parser.add_argument("--input-file", help="Fichero de entrada con la lista de repositorios", required=True)
     parser.add_argument("--branch", required=True, help="Nombre de la rama (por defecto la principal)")
@@ -22,7 +65,21 @@ def get_arguments() -> dict:
     return vars(parser.parse_args())
    
 
-def read_file(file_path : str) -> pd.DataFrame:
+def read_file(file_path: str) -> pd.DataFrame:
+    """
+    Reads an Excel file and returns its content as a pandas DataFrame.
+
+    Parameters:
+    file_path (str): The path to the Excel file to be read.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the data from the specified Excel file,
+                  with specified columns and without any missing values.
+
+    Raises:
+    FileNotFoundError: If the specified file does not exist.
+    ValueError: If the file cannot be read or the specified columns are not found.
+    """
     df = pd.read_excel(
         file_path, 
         usecols=[col.strip() for col in config['excel'].get('cabeceras').split(",")], 
@@ -32,6 +89,17 @@ def read_file(file_path : str) -> pd.DataFrame:
     return df
 
 def get_repo_name(url: str) -> str:
+    """
+    Extracts the repository name from a given GitHub URL.
+
+    Args:
+        url (str): The URL of the GitHub repository.
+
+    Returns:
+        str: The repository name in the format 'owner/repo' if the URL is valid,
+             otherwise an empty string.
+    """
+    
     print('Obteniendo nombre del repositorio de la URL:', url)
     result =  re.search(r"github\.com/([^/]+)/([^/]+)", url)
     if result:
@@ -39,7 +107,21 @@ def get_repo_name(url: str) -> str:
     else:
         return ""
     
-def check_directory(repo: str, directory: str, branch: str, token: str) -> bool:
+def check_directory(repo: str, directory: str, branch: str, token: str) -> str:
+    """
+    Checks if a specific directory exists in a given GitHub repository and branch.
+
+    Args:
+        repo (str): The full name of the repository (e.g., 'owner/repo').
+        directory (str): The path of the directory to check.
+        branch (str): The branch name to check in the repository.
+        token (str): The GitHub personal access token for authentication.
+
+    Returns:
+        str: A message indicating the result of the check. It can indicate
+             whether the directory exists, if it is found in an open pull request,
+             or if the repository or directory was not found.
+    """
     headers = {
         'Accept' : 'application/vnd.github.v3+json',
         'Authorization' : f'token {token}' if token else ''
@@ -66,11 +148,42 @@ def check_directory(repo: str, directory: str, branch: str, token: str) -> bool:
     
 
 def current_path(path) -> str:
+    """
+    Generate a new file path based on the provided path.
+
+    This function takes a file path, extracts the file name and its extension,
+    and returns a new path with the same name but with '_estado' appended before
+    the file extension. The new path is relative to the current directory.
+
+    Args:
+        path (str): The original file path.
+
+    Returns:
+        str: The modified file path with '_estado' appended to the file name.
+    """
     name, extension = os.path.splitext(os.path.basename(path))
     return os.path.join('./', f"{name}_estado{extension}")
     
 
-def main():
+def main() -> None:
+    """
+    Main function to execute the script.
+
+    This function initializes the configuration, retrieves command-line arguments,
+    reads an input file into a DataFrame, checks the status of directories based on
+    the repository names and other parameters, and then saves the results to an Excel file.
+
+    The following steps are performed:
+    1. Initialize configuration settings.
+    2. Get command-line arguments.
+    3. Read the input file into a DataFrame.
+    4. Apply a function to each row of the DataFrame to determine the status of directories.
+    5. Print the results to the console.
+    6. Save the modified DataFrame to an Excel file.
+
+    Returns:
+        None
+    """
     init_config()
     kwargs = get_arguments()
     df = read_file(kwargs.get('input_file'))
