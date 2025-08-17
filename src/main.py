ea5343ff0b2f4f94b553c9126f089e7e091a7bc6
Adasat Torres de León <adasat.torres.dev@gmail.com>
2025-08-17 11:41:21 +0100

    [FIX] no tenia en cuenta los PR con nomenclatura ADD

--- src/main.py
+++ src/main.py
@@ -42,8 +42,7 @@ def init_config() -> None:
     with importlib.resources.path('src', 'config.ini') as config_path:
         config.read(config_path)
         print("Configuración cargada:", config.sections())
-    
-
+        
 def get_arguments() -> dict:
     """
     Parses command-line arguments for the GitHub repository verification script.
@@ -54,12 +53,12 @@ def get_arguments() -> dict:
 
     Returns:
         dict: A dictionary containing the parsed command-line arguments.
-            - 'input-file': The input file with the list of repositories (str).
+            - 'file': The input file with the list of repositories (str).
             - 'branch': The name of the branch (str).
             - 'token': The personal access token for GitHub (str).
     """
     parser = argparse.ArgumentParser(description="Verifica directorios en repositorios de GitHub desde un fichero y exporta resultados a Excel")
-    parser.add_argument("--input-file", help="Fichero de entrada con la lista de repositorios", required=True)
+    parser.add_argument("--file", help="Fichero de entrada con la lista de repositorios", required=True)
     parser.add_argument("--branch", required=True, help="Nombre de la rama (por defecto la principal)")
     parser.add_argument("--token", required=True, help="Token de acceso personal de GitHub")
     return vars(parser.parse_args())
@@ -82,7 +81,7 @@ def read_file(file_path: str) -> pd.DataFrame:
     """
     df = pd.read_excel(
         file_path, 
-        usecols=[col.strip() for col in config['excel'].get('cabeceras').split(",")], 
+        usecols=[col.strip() for col in config['options'].get('headers').split(",")], 
         engine="openpyxl"
     ).dropna()
     print("Fichero leído correctamente:", file_path)
@@ -140,9 +139,12 @@ def check_directory(repo: str, directory: str, branch: str, token: str) -> str:
             response_pull = requests.get(url_pull, headers=headers, stream=True)
             if response_pull.status_code == 200:
                 pulls = response_pull.json()
-                pr = list(filter(lambda pr: f"[{branch}][MIG]{directory}".lower() in pr["title"].lower().replace(" ", ""), pulls))
+                pattern = rf"\[{branch}\]\[(ADD|MIG)\]{directory}".lower()
+
+                #pr = list(filter(lambda pr: f"[{branch}][MIG]{directory}".lower() in pr["title"].lower().replace(" ", ""), pulls))
+                pr = list(filter(lambda pr: re.search(pattern, pr["title"].lower().replace(" ", "")), pulls))
                 if pr:
-                    return f"En PR: {pr[0]['html_url']}"
+                    return f"PR: {pr[0]['html_url']}"
             url_pull = response_pull.links.get('next', {}).get('url', None)
     return """Repositorio o addon no encontrado, revisar manualmente."""
     
@@ -186,11 +188,11 @@ def main() -> None:
     """
     init_config()
     kwargs = get_arguments()
-    df = read_file(kwargs.get('input_file'))
+    df = read_file(kwargs.get('file'))
     df['status'] = df.apply(
         lambda row: check_directory(
-            get_repo_name(row['Sitio web']), 
-            row["Nombre técnico"],
+            get_repo_name(row[config['options'].get('column_url_github')]), 
+            row[config['options'].get('column_dir_name')],
             kwargs['branch'],
             kwargs['token']
             ),
@@ -198,8 +200,8 @@ def main() -> None:
     )
     print("Resultados obtenidos:")
     print(df)
-    print("Guardando resultados en:", current_path(kwargs.get('input_file')))
-    df.to_excel(current_path(kwargs.get('input_file')) , index=True)
+    print("Guardando resultados en:", current_path(kwargs.get('file')))
+    df.to_excel(current_path(kwargs.get('file')) , index=True)
         
         
     
